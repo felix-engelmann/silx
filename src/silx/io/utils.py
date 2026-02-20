6164495921936d126e05da174ae46a014bffc747
Felix Engelmann <fe-github@nlogn.org>
2026-02-20 15:09:25 -0500

    h5pyd compatibility

--- src/silx/io/utils.py
+++ src/silx/io/utils.py
@@ -608,7 +608,10 @@ def _open_url_with_h5pyd(url: str):
     # Retrieve configured HSDS endpoint if any
     default_endpoint = os.environ.get("H5SERV_ENDPOINT", None)
     if default_endpoint is None:
-        cfg = h5pyd.Config()
+        if hasattr(h5pyd, "get_config"):
+            cfg = h5pyd.get_config()
+        else:
+            cfg = h5pyd.Config()
         if "hs_endpoint" in cfg:
             default_endpoint = cfg["hs_endpoint"].rstrip("/")
     else:
